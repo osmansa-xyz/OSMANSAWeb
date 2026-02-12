9002d2e4c0c696cce28b689af57a20340419a4fe
osmansa-xyz <osmansakotabengkulu@gmail.com>
2026-02-12 14:43:17 +0700

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,6 @@ import { Hero } from './components/Hero';
 import { VisionMission } from './components/VisionMission';
 import { OrganizationalStructure } from './components/OrganizationalStructure';
 import { Departments } from './components/Departments';
-import { SmartCalendar } from './components/SmartCalendar';
 import { Contacts} from './components/Contacts';
 import { Footer } from './components/Footer';
 
@@ -16,7 +15,6 @@ export default function App() {
         <VisionMission />
         <OrganizationalStructure />
         <Departments />
-        <SmartCalendar />
         <Contacts />
       </main>
       <Footer />
